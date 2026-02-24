c193063a4f7aa27a81ad168a3522f9940f7d5a70
hrishd <hrishd@abv.bg>
2026-02-24 22:58:53 +0200

    Empty adj matrix edge case

--- TravelingSalesmanProblem_GeneticAlgorithm/Genome.h
+++ TravelingSalesmanProblem_GeneticAlgorithm/Genome.h
@@ -18,27 +18,30 @@ public:
 		path.reserve(numCities);
 	}
 
-    void generatePath(const std::vector<std::vector<int>>& adjMat, int startCity)
+    void generatePath(const std::vector<std::vector<int>>& adjMat)
     {
         int N = adjMat.size();
 
         path.clear();
 
+        //Empty adj matrix edge case
+        if (N == 0)
+        {
+            dist = 0;
+            return;
+        }
+
         // 1. Fill with all cities
         for (int i = 0; i < N; ++i)
             path.emplace_back(i);
 
-        // 2. Move startCity to front
-        auto it = std::find(path.begin(), path.end(), startCity);
-        std::iter_swap(path.begin(), it);
-
-        // 3. Shuffle everything except first element
+        // 2. Shuffle everything except first element since city 0 is the start
         static std::random_device rd;
         static std::mt19937 gen(rd());
 
         std::shuffle(path.begin() + 1, path.end(), gen);
 
-        // 4. Compute distance
+        // 3. Compute distance
         dist = 0;
 
         for (int i = 0; i < N - 1; ++i)
