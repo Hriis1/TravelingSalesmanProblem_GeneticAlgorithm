6ef31d8cb22cb975a14e39ebc480d4c0b19dd061
hrishd <hrishd@abv.bg>
2026-02-24 23:29:44 +0200

    Traveling Salesman problem solver class initial funcs

---
+++ TravelingSalesmanProblem_GeneticAlgorithm/TravelingSalesmanProblem.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <climits>
+
+#include "Genome.h"
+
+class TravelingSalesmanProblem
+{
+public:
+	TravelingSalesmanProblem(const std::vector<std::vector<int>>& adjMat, int ng, int npop, float pc, float pm)
+		:_adjMat(&adjMat), _NG(ng), _NPOP(npop), _PC(pc), _PM(pm), _currSolution(adjMat.size())
+	{
+		_currSolution.dist = INT_MAX;
+	}
+
+	void reinit(const std::vector<std::vector<int>>& adjMat, int ng, int npop, float pc, float pm)
+	{
+		_adjMat = &adjMat;
+		_NG = ng;
+		_NPOP = npop;
+		_PC = pc;
+		_PM = pm;
+		_currSolution = Genome(adjMat.size());
+		_currSolution.dist = INT_MAX;
+	}
+
+	void solve()
+	{
+
+	}
+
+	//getters
+	int getCurrSolutionDist() const
+	{
+		return _currSolution.dist;
+	}
+
+	const std::vector<int>& getCurrSolutionPath() const
+	{
+		return _currSolution.path;
+	}
+
+private:
+	const std::vector<std::vector<int>>* _adjMat = nullptr;
+	int _NG = 0;
+	int _NPOP = 0;
+	float _PC = 0.0f;
+	float _PM = 0.0f;
+	Genome _currSolution;
+};
