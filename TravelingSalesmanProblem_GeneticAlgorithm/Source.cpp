e73375ba9437528b015dfa7f094d069ebe697ae6
hrishd <hrishd@abv.bg>
2026-02-26 14:21:01 +0200

    output path func

--- TravelingSalesmanProblem_GeneticAlgorithm/Source.cpp
+++ TravelingSalesmanProblem_GeneticAlgorithm/Source.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <vector>
+
+void outputPath(const std::vector<int>& path)
+{
+	const int sz = path.size();
+
+	if (sz == 0)
+		return;
+
+	//Output path
+	for (size_t i = 0; i < sz; i++)
+	{
+		std::cout << path[i] << " -> ";
+	}
+
+	//Return to start city
+	if (sz > 1) 
+	{
+		std::cout << path[0];
+	}
+
+	//New line
+	std::cout << std::endl;
+}
 
 int main() 
 {
